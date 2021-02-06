b0965c8498590a971300da89404812bd18ece6fb
Nolm <nolm88@yandex.ru>
2021-02-06 16:53:13 +0300

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ function Header(props) {
 
   return (
     <header className={`header ${props.isOpenMob ? 'header__background' : ''} ${props.theme ? 'header_saved-news' : 'header_main'}`}>
-      <Link  className="header__link" to="/"><div to="/" className={`${props.theme ? 'header__logo' : 'header__logo-main'} ${props.isOpenMob ? 'header__logo-main' : 'header__logo' }`}>NewsExplorer</div>
+      <Link  className="header__link" to="/"><div className={`${props.theme ? 'header__logo' : 'header__logo-main'} ${props.isOpenMob ? 'header__logo-main' : 'header__logo' }`}>NewsExplorer</div>
       </Link>
       <button type="button" onClose={props.onClose} onClick={props.open} className={`header__burger ${props.loggedIn ? 'header__burger-img_black' : 'header__burger-img_white'} ${props.isOpenMob ? 'header__burger-close' : ''}`}></button>
       <Navigation 
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,7 +7,7 @@ function SearchForm() {
       <h1 className="searchform__title">Что творится в мире?</h1>
       <p className="searchform__subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
       <form className="searchform__form">
-        <input className="searchform__input"></input>
+        <input className="searchform__input" placeholder="Введите тему новости" autoComplete="on" required></input>
         <button className="searchform__button">Искать</button>
       </form>
     </section>
